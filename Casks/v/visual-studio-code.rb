8c83e61989fd6d478820f4095c3b63acfc39f499
Joe Nyland <7666402+JoeNyland@users.noreply.github.com>
2026-02-20 18:16:34 +0000

    Update VS Code to the latest version that is supported on macOS Big Sur

--- Casks/v/visual-studio-code.rb
+++ Casks/v/visual-studio-code.rb
@@ -1,9 +1,9 @@
 cask "visual-studio-code" do
   arch arm: "darwin-arm64", intel: "darwin"
 
-  version "1.87.2"
-  sha256 arm:   "cbaff07673e4d86974a78917a95bcca31e0f33249a41c47705493cb99b99534b",
-         intel: "ed3b1fc13cf46069529902e2c1ce754284d2d7b6078d5b188bf79f81639527aa"
+  version "1.106.3"
+  sha256 arm:   "35dd438808dde1dd1f65490ffe7713ed64102324c0809efbec0b4eb2809b218b",
+         intel: "c41872149a205f3a3be3e5d3a8f04920407a0762531e607f78dc93f4d4813cda"
 
   url "https://update.code.visualstudio.com/#{version}/#{arch}/stable"
   name "Microsoft Visual Studio Code"

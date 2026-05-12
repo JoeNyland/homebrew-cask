a0e261da640d0ea1227dbb11826eb986c8314c2a
Joe Nyland <7666402+JoeNyland@users.noreply.github.com>
2026-05-12 09:30:05 +0100

    Update Plexamp
    Note: Plexamp will autoupdate itself to the latest version, regardless of whether that version is supported for the current OS. Therefore, on older OSes, the application bundle must be 'locked' to prevent the update.

--- Casks/p/plexamp.rb
+++ Casks/p/plexamp.rb
@@ -1,9 +1,10 @@
 cask "plexamp" do
-  arch arm: "-arm64"
+  arch arm: "-arm64",
+       intel: "-x64"
 
-  version "4.10.1"
-  sha256 arm:   "9fbd45ca92e36adc984a21eda363bde14aa7fe7706d79c3de74e68bf63538f2d",
-         intel: "507e05584149694e338c621686a9662dae814ec98e74bd58af7f4abc7875f481"
+  version "4.12.4"
+  sha256 arm:   "313453eefdb6b8a295800e6530442ad0527884ed14533dfb92166b6bdc2dcca9",
+         intel: "dc4358f8224b0e22658adad6929f98e791b9a6136907e7d63240921ac8cd26d8"
 
   url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}#{arch}.dmg",
       verified: "plexamp.plex.tv/"
